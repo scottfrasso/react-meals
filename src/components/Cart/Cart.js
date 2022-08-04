437e121dc45a60c37ab1bb2052d44d2cc09bae33
Scott Frasso <scottfrasso@gmail.com>
2022-08-04 15:21:53 +0200

    Made the cart modal closeable.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,20 +5,22 @@ const Cart = (props) => {
   const cartItems = (
     <ul className={classes['cart-items']}>
       {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => {
-        return <li>{item.name}</li>
+        return <li key={item.id}>{item.name}</li>
       })}
     </ul>
   )
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <buton className={classes['button--alt']}>Close</buton>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
