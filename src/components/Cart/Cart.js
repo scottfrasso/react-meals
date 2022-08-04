89ac77afdfda0d4b7a0e086aa545985c78d0049b
Scott Frasso <scottfrasso@gmail.com>
2022-08-04 17:38:18 +0200

    Wired up the state/context to the add button.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,34 @@
+import { useContext } from 'react'
+
 import Modal from '../UI/Modal'
+import CartContext from '../../store/cart-context'
+import CartItem from './CartItem'
+
 import classes from './Cart.module.css'
 
 const Cart = (props) => {
+  const cartContext = useContext(CartContext)
+
+  const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`
+  const hasItems = cartContext.items.length > 0
+
+  const cartItemRemoveHandler = (id) => {}
+
+  const cartItemAddHandler = (item) => {}
+
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => {
-        return <li key={item.id}>{item.name}</li>
+      {cartContext.items.map((item) => {
+        return (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item.id)}
+          />
+        )
       })}
     </ul>
   )
@@ -15,13 +38,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   )
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -4,17 +4,33 @@ import CartContext from './cart-context'
 
 const defaultCartState = {
   items: [],
-  amount: 0,
+  totalAmount: 0,
 }
 
 const cartReducer = (state, action) => {
   if (action.type == 'ADD') {
-    const updatedItems = state.items.concat(action.item)
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    )
+    const existingCartItem = state.items[existingCartItemIndex]
+    let updatedItems
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      }
+      updatedItems = [...state.items]
+      updatedItems[existingCartItemIndex] = updatedItem
+    } else {
+      updatedItems = state.items.concat(action.item)
+    }
+
     return {
       items: updatedItems,
-      amount: updatedTotalAmount,
+      totalAmount: updatedTotalAmount,
     }
   }
   return defaultCartState
