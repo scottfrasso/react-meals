c7164059c936161f0ef441e5c0cbe1861c34e14c
Scott Frasso <scottfrasso@gmail.com>
2022-08-04 16:00:11 +0200

    Added a cart store.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 
-import Header from './components/layout/Header'
+import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
 import Cart from './components/Cart/Cart'
+import CartProvider from './store/CartProvider'
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false)
@@ -16,13 +17,13 @@ function App() {
   }
 
   return (
-    <React.Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} onHideCart={hideCartHandler} />
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   )
 }
 
