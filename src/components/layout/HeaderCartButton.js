3da94b68df7c522a3f36eabf2299aa4ba285c967
Scott Frasso <scottfrasso@gmail.com>
2022-08-05 14:28:42 +0200

    Make the Cart bump when items are added.

--- src/components/layout/HeaderCartButton.js
+++ src/components/layout/HeaderCartButton.js
@@ -1,18 +1,39 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 import CartIcon from '../Cart/CartIcon'
 import CartContext from '../../store/cart-context'
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = (props) => {
+  const [buttonIsHighlited, setButtonIsHighlited] = useState()
   const cartContext = useContext(CartContext)
+  const { items } = cartContext
 
-  const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => {
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount
   }, 0)
 
+  const buttonClasses = `${classes.button} ${
+    buttonIsHighlited ? classes.bump : ''
+  }`
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+    setButtonIsHighlited(true)
+
+    const timer = setTimeout(() => {
+      setButtonIsHighlited(false)
+    }, 300)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
