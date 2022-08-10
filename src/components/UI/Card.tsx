b22fe64f733108990f97a4f33a90a53899acc892
Scott Frasso <scottfrasso@gmail.com>
2022-08-10 15:43:20 +0200

    Changed Card, App, and index over to typescript.

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -1,6 +1,12 @@
+import React from 'react'
+
 import classes from './Card.module.css'
 
-const Card = (props) => {
+type Props = {
+  children?: React.ReactNode
+}
+
+const Card = (props: Props) => {
   return <div className={classes.card}>{props.children}</div>
 }
 
