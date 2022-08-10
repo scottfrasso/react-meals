ba89736db28bf21f9d094a17c881d6e3c18f294f
Scott Frasso <scottfrasso@gmail.com>
2022-08-10 15:09:56 +0200

    Switched over MealItemForm and Input to typescript.

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 import classes from './Input.module.css'
 
-const Input = React.forwardRef((props, ref) => {
+type Props = {
+  label: string
+  input: {
+    id: string
+    type: string
+    min: string
+    max: string
+    step: string
+    defaultValue: string
+  }
+}
+const Input = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
